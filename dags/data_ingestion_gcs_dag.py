c4b90d85a6e7b93b09246ef54bddfe90b760e343
mukhamadazistholib <mukhamadazistholib278@gmail.com>
2023-03-09 18:09:01 +0700

    solve minor code

--- dags/data_ingestion_gcs_dag.py
+++ dags/data_ingestion_gcs_dag.py
@@ -1,28 +1,27 @@
 import os
+import glob
 import logging
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+from airflow.models import Variable
 
 from google.cloud import storage
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
 import pyarrow.csv as pv
 import pyarrow.parquet as pq
 
-PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
-BUCKET = os.environ.get("GCP_GCS_BUCKET")
-PROJECT_ID = "data-fellowship-379703"
-BUCKET = "playgrounddata-bucket"
+PROJECT_ID = Variable.get("PROJECT_ID")
+BUCKET = Variable.get("BUCKET_NAME")
 
-dataset_file = "yellow_tripdata_2021-1.csv"
-# dataset_file = "yellow_tripdata_2021-01.csv"
-dataset_url = f"https://s3.amazonaws.com/nyc-tlc/trip+data/{dataset_file}"
+#dataset_file = "yellow_tripdata_2022.csv"
+#dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-*.parquet"
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 # path_to_local_home = "/opt/airflow/"
-parquet_file = dataset_file.replace('.csv', '.parquet')
-BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
+#parquet_file = dataset_file.replace('.csv', '.parquet')
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET",'Testing')
 
 
 def format_to_parquet(src_file):
@@ -34,7 +33,7 @@ def format_to_parquet(src_file):
 
 
 # NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
-def upload_to_gcs(bucket, object_name, local_file):
+def upload_to_gcs(bucket, object_dir, local_glob):
     """
     Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
     :param bucket: GCS bucket name
@@ -42,6 +41,8 @@ def upload_to_gcs(bucket, object_name, local_file):
     :param local_file: source path & file-name
     :return:
     """
+
+    # logging.info(f"bucket nama: {bucket}")
     # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
     # (Ref: https://github.com/googleapis/python-storage/issues/74)
     storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
@@ -51,8 +52,10 @@ def upload_to_gcs(bucket, object_name, local_file):
     client = storage.Client()
     bucket = client.bucket(bucket)
 
-    blob = bucket.blob(object_name)
-    blob.upload_from_filename(local_file)
+    for ifile in glob.glob(local_glob):
+        filename = os.path.basename(ifile)
+        blob = bucket.blob(f"{object_dir}/{filename}")
+        blob.upload_from_filename(ifile)
 
 
 default_args = {
@@ -74,40 +77,41 @@ with DAG(
 
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
+        #bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
+        bash_command=f"for m in {{01..3}}; do wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-$m.parquet -P {path_to_local_home}; done"
     )
 
-    format_to_parquet_task = PythonOperator(
-        task_id="format_to_parquet_task",
-        python_callable=format_to_parquet,
-        op_kwargs={
-            "src_file": f"{path_to_local_home}/{dataset_file}",
-        },
-    )
+    # format_to_parquet_task = PythonOperator(
+    #     task_id="format_to_parquet_task",
+    #     python_callable=format_to_parquet,
+    #     op_kwargs={
+    #         "src_file": f"{path_to_local_home}/{dataset_file}",
+    #     },
+    # )
 
     local_to_gcs_task = PythonOperator(
         task_id="local_to_gcs_task",
         python_callable=upload_to_gcs,
         op_kwargs={
             "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
+            "object_dir": f"raw",
+            "local_glob": f"{path_to_local_home}/*.parquet",
         },
     )
 
     bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-        task_id="bigquery_external_table_task",
-        table_resource={
-            "tableReference": {
-                "projectId": PROJECT_ID,
-                "datasetId": BIGQUERY_DATASET,
-                "tableId": "external_table",
-            },
-            "externalDataConfiguration": {
-                "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
-            },
-        },
-    )
+         task_id="bigquery_external_table_task",
+         table_resource={
+             "tableReference": {
+                 "projectId": PROJECT_ID,
+                 "datasetId": BIGQUERY_DATASET,
+                 "tableId": "yellowtaxi_datatrip",
+             },
+             "externalDataConfiguration": {
+                 "sourceFormat": "PARQUET",
+                 "sourceUris": [f"gs://{BUCKET}/raw/yellow_tripdata_2022-01.parquet"],
+             },
+         },
+     )
 
-    download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> bigquery_external_table_task
+    download_dataset_task >> local_to_gcs_task >> bigquery_external_table_task
